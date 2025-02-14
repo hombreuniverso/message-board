e5c2a496877e5c69da987c810bf4b84579a51058
Colin Karr <67452254+hombreuniverso@users.noreply.github.com>
2025-02-14 17:39:10 -0400

    Change port in app.js
    Change port in app.js in order to
    deploy on Koyeb:
    
    const port = process.env.port || 3000;
    
    This will allow Koyeb to set the port
    dynamically and will default to 3000
    if not set

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const detailsRouter = require("./routes/detailsRouter");
 const app = express();
 
 //Create variable to store port value
-const port = 3000;
+const port = process.env.port || 3000;
 
 //Create server
 app.listen(port, (req, res) => {
